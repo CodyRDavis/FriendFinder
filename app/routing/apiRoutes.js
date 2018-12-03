3ad0aae4ed7bc7173dd774243b8c48b359920b84
Cody Davis <Cody.1337@gmail.com>
2018-12-03 12:52:31 -0600

    starting the logic for matching.

--- app/routing/apiRoutes.js
+++ app/routing/apiRoutes.js
@@ -3,26 +3,45 @@ const express = require('express');
 const router = express.Router();
 
 //route to get back the friends obj
-router.get('/api/:name', function(req, res){
+router.get('/api', function(req, res){
     res.send(friends);
 });
 
 //route to add new friend
 //
 router.post('/api', function(req, res){
-    //insert new friend into friends.
-    console.log(JSON.stringify(req.body));
+
+    //getting current users scores for easy refrence
+    currentUserScores = []
+    for (let i = 0; i < req.body.score.length; i++){
+        currentUserScores.push(req.body.score[i]);
+    }
+    console.log ("current user's scores: ", currentUserScores);
+
+    //comparing scores to find best match
+    bestMatch = []
+    for (let i =0; i<friends.length; i++){
+        console.log (friends[i]);
+        let total = 0;
+        for ( let j = 0; j<friends[i].score.length; j++){
+            console.log ("Comparing scores:", j)
+            total += Math.abs(currentUserScores[i] - friends[i].score[j]);
+        }
+        bestMatch.push(total);
+    }
+    console.log("Best Matches: " + bestMatch);
+
     friends.push(req.body);
     res.send(friends);
 });
 
 //route to update existing friend
-router.put('/api/:name',function (req,res){
+router.put('/api',function (req,res){
     res.send("");
 });
 
 //rout to delete existing friend
-router.delete('/api/:name', function (req,res){
+router.delete('/api', function (req,res){
     res.send("");
 });
 
