33210596a4c1329f3376e04432096a5023133e70
Cody Davis <codydavis@codys-imac.lan>
2018-12-02 20:14:15 -0600

    new routing for html and api

--- app/routing/htmlRoutes.js
+++ app/routing/htmlRoutes.js
@@ -1,21 +1,7 @@
 const api = require('./apiRoutes');
-module.exports = {
-    route : function(app){
-        
-        app.get('/survey', function(req, res){
-            res.sendFile('/app/public/survey.html', {root: './'});
-        });
-        app.get('/', function(req, res){
-            console.log("Sending: ../public/index.html");
-            res.sendFile('/app/public/index.html', { root: "./"});
-        });
-        app.get('/survey/:name', function(req, res){
-            console.log("Received request for friends results, forwarding to api");
-            api.route(app);
-        });
-        //default path, incase of bad address
-        app.get('*', function(req,res){
-            res.sendFile('/app/public/index.html', {root: './'})
-        });
-    } 
-}
+const express = require('express');
+const router = express.Router();
+
+router.get("/" function(req, res))
+
+module.exports = router;
