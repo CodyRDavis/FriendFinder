9bc595d71742692b3194bd73795f3eef0b72dbca
Cody Davis <Cody.1337@gmail.com>
2018-12-04 10:04:26 -0600

    standarized quotes

--- app/routing/htmlRoutes.js
+++ app/routing/htmlRoutes.js
@@ -4,17 +4,17 @@ const router = express.Router();
 
 //routing for homepage
 router.get("/", function(req, res){
-    res.sendFile(path.join(__dirname, '..',"public",'index.html'));
+    res.sendFile(path.join(__dirname, '..','public','index.html'));
 });
 //route for survey
 router.get("/survey", function(req, res){
-    res.sendFile(path.join(__dirname, '..',"public",'survey.html'));
+    res.sendFile(path.join(__dirname, '..','public','survey.html'));
 });
 
 //catch all for not explicit routes
 //seperate from / incase of desired 404 page.
 router.get('*', function(req,res){
-    res.sendFile(path.join(__dirname, '..',"public",'index.html'));
+    res.sendFile(path.join(__dirname, '..','public','index.html'));
 })
 
 module.exports = router;
