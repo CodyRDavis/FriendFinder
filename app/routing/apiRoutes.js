e4872a940d5ad171a6def6cbcb93039bfb8325f2
Cody Davis <codydavis@codys-imac.lan>
2018-12-03 18:46:25 -0600

    apiroutes updated removing bad logic.

--- app/routing/apiRoutes.js
+++ app/routing/apiRoutes.js
@@ -20,29 +20,6 @@ router.get('/:id', function(req,res){
         }
     }
 
-    //bypassing 0 since 0 is used as first best match above
-    for(let i = 1; i < friends.length; i++){
-        if(friends[i].id != userId){
-            console.log("hmm...is " + friends[i].name + " a good friend for you?");
-
-            //calculating score differences
-            let totalBestMatch = 0;
-            let totalNewFriend = 0;
-            for(let j = 0; j< bestMatch.score.length; j++){
-                console.log(bestMatch.score[j], user.score[j])
-                totalBestMatch += Math.abs((bestMatch.score[j] - user.score[j]));
-                console.log(bestMatch.name, totalBestMatch);
-                totalNewFriend += Math.abs((friends[i].score[j] - user.score[j]));
-                console.log(friends[i].name, totalNewFriend)
-            }
-
-            //checking to see if friend at this postiion has more comparable score
-            if(totalBestMatch > totalNewFriend){
-                bestMatch = friends[i];
-            }
-        }
-    }
-
     res.send(bestMatch);
 });
 
