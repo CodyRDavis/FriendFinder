5e2f440cf72eb9c68683a0d5aaf4f4e7db38a1b3
Cody Davis <codydavis@codys-imac.lan>
2018-12-03 18:45:58 -0600

    i cant figure out friend logic right now. burnt out.

--- app/data/friends.js
+++ app/data/friends.js
@@ -1,5 +1,6 @@
 let friends =[
     {
+        "id": 0,
         "name": "Pikachu",
         "photo": "https://s1.zerochan.net/Pikachu.600.1668912.jpg",
         "score": [
--- app/routing/apiRoutes.js
+++ app/routing/apiRoutes.js
@@ -7,38 +7,49 @@ router.get('/', function(req, res){
     res.send(friends);
 });
 
+router.get('/:id', function(req,res){
+    const userId = req.params.id;
+    let user = friends[0];
+    let bestMatch = friends[0];
+
+    //getting ID's stats
+    for(let i = 0; i<friends.length; i++){
+        if(friends[i].id === userId){
+            user = friends[i];
+            break;
+        }
+    }
+
+    //bypassing 0 since 0 is used as first best match above
+    for(let i = 1; i < friends.length; i++){
+        if(friends[i].id != userId){
+            console.log("hmm...is " + friends[i].name + " a good friend for you?");
+
+            //calculating score differences
+            let totalBestMatch = 0;
+            let totalNewFriend = 0;
+            for(let j = 0; j< bestMatch.score.length; j++){
+                console.log(bestMatch.score[j], user.score[j])
+                totalBestMatch += Math.abs((bestMatch.score[j] - user.score[j]));
+                console.log(bestMatch.name, totalBestMatch);
+                totalNewFriend += Math.abs((friends[i].score[j] - user.score[j]));
+                console.log(friends[i].name, totalNewFriend)
+            }
+
+            //checking to see if friend at this postiion has more comparable score
+            if(totalBestMatch > totalNewFriend){
+                bestMatch = friends[i];
+            }
+        }
+    }
+
+    res.send(bestMatch);
+});
+
 //route to add new friend
 //
 router.post('/', function(req, res){
-
-    //getting current users scores for easy refrence
-    currentUserScores = []
-    for (let i = 0; i < req.body.score.length; i++){
-        currentUserScores.push(req.body.score[i]);
-    }
-    console.log ("current user's scores: ", currentUserScores);
-
-    //comparing scores to find best match
-    bestMatch = {
-        "name": "",
-        "scoreDiff": 999
-    };
-    for (let i =0; i<friends.length; i++){
-        console.log (friends[i]);
-        let total = 0;
-        for ( let j = 0; j<friends[i].score.length; j++){
-            console.log ("Comparing scores:", j)
-            total += Math.abs(currentUserScores[i] - friends[i].score[j]);
-        }
-        
-        //checking to see who is the lowest difference
-        if (total < bestMatch.scoreDiff){
-            bestMatch.name = friends[i].name;
-            bestMatch.scoreDiff = total;
-        }
-    }
-    console.log("Best Matches: ",bestMatch);
-
+    req.body.id = friends.length;
     friends.push(req.body);
     res.send(friends);
 });
